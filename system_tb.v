97b16fd3a2342e192c9fc538839b2545251cdc25
Addison Elliott <addison.elliott@gmail.com>
2018-11-03 09:26:12 -0500

    Created testbench that has not been tested yet

---
+++ system_tb.v
@@ -0,0 +1,27 @@
+`timescale 1 ns / 1 ps
+
+module system_tb();
+    wire [3:0] LEDs;
+
+    reg reset, clock;
+    reg [3:0] switches;
+
+    // UUT = Unit Under Test
+    system UUT(LEDs, switches, reset, clock);
+
+    initial begin
+        reset <= 1'b1;
+        clock <= 1'b0;
+        switches <= 4'b0000;
+
+        forever begin
+            // 5ns = 1/2 * period for 100MHz clock
+            #5 clock <= ~clock;
+        end
+    end
+
+    initial begin
+        #7 reset <= 1'b0;
+        #100 $finish();
+    end
+endmodule
