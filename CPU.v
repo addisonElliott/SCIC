57c13bdaa25e53c0063cfe3e45a6455ce6b7a883
Addison Elliott <addison.elliott@gmail.com>
2018-11-15 12:59:49 -0600

    Update SDC for max capacitance

--- CPU.v
+++ CPU.v
@@ -19,13 +19,8 @@ module CPU(output [31:0] data_out, output[15:0] address, output we, input[31:0] 
 
     // instantiate full adder
     // wire [31:0] adderOut;
-    // wire [31:0] adderIn1;
-    // wire [31:0] adderIn2;
 
-    // assign adderIn1 = data_in;
-    // assign adderIn2 = AC;
-
-    // fullAdder_32bit_struct fullAdder(adderIn1, adderIn2, adderOut);
+    // fullAdder_32bit_struct fullAdder(AC, data_in, adderOut);
 
     always @(posedge clock) begin
         // Active HIGH reset
--- memory_controller.v
+++ memory_controller.v
@@ -7,7 +7,7 @@ module memory_controller(output [31:0] data_out, output [3:0] io_out, input [31:
     //      Range       Item    Size (words)    Binary Range
     //      0000-001F   ROM     32              0000 0000 0000 0000 -> 0000 0000 0001 1111
     //      0020-003F   I/O     32              0000 0000 0010 0000 -> 0000 0000 0011 1111
-    //      0800-0FFF   RAM     2048            0000 1000 0000 0000 -> 0000 1111 1111 1111
+    //      0040-005F   RAM     32              0000 0000 0100 0000 -> 0000 0000 0101 1111
     //      Otherwise return 0s
     // Note: Placed the memory on byte boundaries to make it easy to synthesize the logic for this, but I am not sure if these if statements will synthesize that way
     // @(*) is a combinational block but you are not required to use an assign statement for wires
@@ -16,21 +16,7 @@ module memory_controller(output [31:0] data_out, output [3:0] io_out, input [31:
     //      1 = I/O
     //      2 = RAM
     //      3 = Otherwise
-//    always @(*) begin
-//        if (address <= 16'h001f) begin
-//            data_select <= 2'b00;
-//        end
-//        else if (address <= 16'h003F) begin
-//            data_select <= 2'b01;
-//        end
-//        else if (address >= 16'h0800 && address <= 16'h0FFF) begin
-//            data_select <= 2'b10;
-//        end
-//        else begin
-//            data_select <= 2'b11;
-//        end
-//    end
-    assign data_select = address[6:5];
+    assign data_select = ~(|address[15:7]) ? address[6:5] : 2'b11;
 
     // Wires for if a particular memory module is selected
     // This is essentially a behavioral description of a demultiplexor
@@ -41,7 +27,7 @@ module memory_controller(output [31:0] data_out, output [3:0] io_out, input [31:
     // ROM, I/O & RAM module instantiations
     ROM rom_inst(rom_data_out, address[4:0], select_rom);
     io_controller io_controller_inst(io_data_out, io_out, data_in, address[4:0], io_in, select_io, we, clock);
-    RAM ram_inst(ram_data_out, data_in, address[10:0], we, select_ram, clock);
+    RAM ram_inst(ram_data_out, data_in, address[5:0], we, select_ram, clock);
 
     // data_out wire is set to be whichever address range we are speaking to
     // In the case of an unknown address space being specified, the value returned is all zeros
