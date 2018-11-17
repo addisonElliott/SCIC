e8eda2026d74815233ba4e459aaa813974880ad8
Addison Elliott <addison.elliott@gmail.com>
2018-11-17 11:24:27 -0600

    Okay, so it is the RAM somehow

--- CPU.v
+++ CPU.v
@@ -41,12 +41,14 @@ module CPU(output reg [15:0] PC, output reg [31:0] IR, AC, output [31:0] data_ou
                     // Add AC <= AC + mem(IR[15:0])
                     4'b0001: begin
                         // AC <= AC + data_in;
-                        AC <= AC + IR[15:0];
+                        // AC <= AC + IR[15:0];
+                        AC <= data_in;
                     end
 
                     // Shift AC <= AC << mem(IR[15:0])
                     4'b0010: begin
-                        AC <= AC << data_in;
+                        // AC <= AC << data_in;
+                        AC <= data_in;
                     end
 
                     // Shift AC <= AC >> mem(IR[15:0])
