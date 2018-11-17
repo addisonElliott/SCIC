a352b81271923f473bbc8f4393792df924f1ae86
Addison Elliott <addison.elliott@gmail.com>
2018-11-17 12:08:38 -0600

    Chagnes

--- RAM.v
+++ RAM.v
@@ -1,10 +1,11 @@
- module RAM(output reg [31:0] data_out, input [31:0] data_in, input [4:0] address, input we, chip_select, clock);
+ module RAM(output /*reg*/ [31:0] data_out, input [31:0] data_in, input [4:0] address, input we, chip_select, clock);
     // 32 x 32 memory
     reg [31:0] memory [31:0];
     // reg [31:0] memory [0:31];
 
     // Data out is memory address if selected, otherwise unknown
     // assign data_out = chip_select ? memory[address] : 32'bx;
+    assign data_out = memory[address];
 
     always @(negedge clock) begin
         // Only do stuff when chip_select is HIGH
@@ -13,9 +14,9 @@
             if (we) begin
                 memory[address] <= data_in;
             end
-            else begin
-                data_out <= memory[address];
-            end
+            // else begin
+            //     data_out <= memory[address];
+            // end
         end
     end
 endmodule
