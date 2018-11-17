4d9e174eac4ddde1b927b5c1358a80a6a9bb2f15
Addison Elliott <addison.elliott@gmail.com>
2018-11-17 11:51:29 -0600

    Maybe?

--- RAM.v
+++ RAM.v
@@ -1,6 +1,7 @@
  module RAM(output reg [31:0] data_out, input [31:0] data_in, input [4:0] address, input we, chip_select, clock);
     // 32 x 32 memory
-    reg [31:0] memory [0:31];
+    reg [31:0] memory [31:0];
+    // reg [31:0] memory [0:31];
 
     // Data out is memory address if selected, otherwise unknown
     // assign data_out = chip_select ? memory[address] : 32'bx;
