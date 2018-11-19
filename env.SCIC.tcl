1e0c6fd00633ab30a5e982d45e84362aebc3c5d6
Addison Elliott <addison.elliott@gmail.com>
2018-11-19 15:06:38 -0600

    x

--- env.SCIC.tcl
+++ env.SCIC.tcl
@@ -79,8 +79,8 @@ $TCL_DIR/enc/enc_hitkit.tcl \
 # ---------------------------------------------------------------------------
 
 # Provide X and Y dimensions of the core
-set CORE_X 40000
-set CORE_Y 40000
+set CORE_X 80000
+set CORE_Y 80000
 
 # Set the aspect ratio for the layout
 # A values less than 1.0 means wide and not so high!
