849444fd99ff44d874c3d79d3a5c82f9a57cda93
Addison Elliott <addison.elliott@gmail.com>
2018-11-19 11:13:05 -0600

    .

--- SCIC.v
+++ SCIC.v
@@ -7,5 +7,6 @@ module SCIC(output [31:0] AC, output [3:0] LEDs, input [3:0] switches, input res
 
     // Note: reset is active HIGH
     memory_controller memory_controller_inst(data_toCPU, LEDs, data_fromCPU, address, switches, we, clock);
-    CPU cpu_inst(data_fromCPU, address, we, data_toCPU, reset, clock);
+    // CPU cpu_inst(data_fromCPU, address, we, data_toCPU, reset, clock);
+    CPU cpu_inst(AC, data_fromCPU, address, we, data_toCPU, reset, clock);
 endmodule
