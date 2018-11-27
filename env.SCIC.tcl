f156d7863e57124dc220f80198c7f6577993266f
Addison Elliott <addison.elliott@gmail.com>
2018-11-27 15:49:55 -0600

    Still not working

--- env.SCIC.tcl
+++ env.SCIC.tcl
@@ -29,13 +29,12 @@ set MODULE_LIST ""
 # we would like to employ
 #
 # source $env(EDI_TCL_DIR)/defaults.tcl
+source defaults.tcl
 
 # ---------------------------------------------------------------------------
 
 # Point to key source directories
 set BASE_DIR $SRC/$BASENAME
-
-source $BASE_DIR/defaults.tcl
 
 #
 # These files are used for RTL simulations (sim rtl)
