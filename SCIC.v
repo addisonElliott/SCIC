664f81f9c7edc223108e53e1f660bc8d43f6c7e9
Addison Elliott <addison.elliott@gmail.com>
2018-11-28 08:11:35 -0600

    Remove memory controller and attempt to get working

--- SCIC.v
+++ SCIC.v
@@ -5,10 +5,10 @@ module SCIC(output [3:0] LEDs, input [3:0] switches, input reset, clock);
     wire we;
 
     // TODO: Temporarily remove memory controller to see if it causes problems
-    // assign LEDs = 4'b0000;
-    // assign data_toCPU = 32'h0000_000f;
+    assign LEDs = 4'b0000;
+    assign data_toCPU = 32'h0000_000f;
 
     // Note: reset is active HIGH
-    memory_controller memory_controller_inst(data_toCPU, LEDs, data_fromCPU, address, switches, we, clock);
+    // memory_controller memory_controller_inst(data_toCPU, LEDs, data_fromCPU, address, switches, we, clock);
     CPU cpu_inst(data_fromCPU, address, we, data_toCPU, reset, clock);
 endmodule
